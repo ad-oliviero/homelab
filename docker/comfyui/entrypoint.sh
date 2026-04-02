8cac9225e727130705b28053d1a89e1ae622a85f
Adriano Oliviero <adrianoliviero23@gmail.com>
2026-04-02 15:39:08 +0200

    Updated comfyui config

--- docker/comfyui/entrypoint.sh
+++ docker/comfyui/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+OLD_HEAD=$(git rev-parse HEAD)
 git pull origin master
+NEW_HEAD=$(git rev-parse HEAD)
 
-pip install --no-cache-dir -r requirements.txt
+if [ "$OLD_HEAD" != "$NEW_HEAD" ]; then
+    uv pip install --system --no-cache-dir -r requirements.txt
+fi
 
-exec python main.py --listen 0.0.0.0 $CLI_ARGS "$@"
+exec python ./main.py --listen 0.0.0.0 $CLI_ARGS "$@"
